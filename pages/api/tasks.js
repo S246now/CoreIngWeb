6583d711b1014c374199a75bb897ff6a3827961b
S246now <odalisse.arguello@udla.edu.ec>
2023-07-17 20:10:27 -0500

    added GET request for tasks with typeTask name

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -1,10 +1,38 @@
 //for All Tasks
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 async function Handler(req, res) {
-    const taskTime = req.body.time
-    const productivity = req.body.productivity
-    
+  const taskTime = req.body.time
+  const productivity = req.body.productivity
+  //connection
+  const client = await MongoClient.connect(
+    'mongodb+srv://crud:crud123@web.gq8l24z.mongodb.net/?retryWrites=true&w=majority'
+  );
+
+
+  //GET request
+  if (req.method === 'GET') {
+    const db = client.db('core');
+
+    const collection = db.collection('tasks');
+    const data = await collection.find().toArray();
+    //res.status(200).json({ sales: data });
+
+    // Obtein TypeTask for each Task
+    const showInfoTasks = await Promise.all(
+      data.map(async (task) => {
+        const typeTaskCollection = db.collection('typeTask');
+        const type = await typeTaskCollection.findOne({ _id: new ObjectId(task.typeTaskId) });
+        const infoTasks = { ...task, taskName: type ? type.name : 'Tarea NO encontrada' };
+        return infoTasks;
+      })
+    );
+
+    res.status(200).json({ tasks: showInfoTasks });
+  }
+
+  //POST request
+  if (req.method === 'POST') {
     //añade validación
     if (
       !taskTime ||
@@ -15,28 +43,25 @@ async function Handler(req, res) {
       res.status(422).json({ message: 'Invalid input' });
       return;
     }
-
     //cálculo de productividad
-
+    
     //javascript object
     const newTask = {
       taskTime,
       productivity,
     };
-    console.log(newTask);//chi
+    //console.log(newTask);
 
-    if (req.method === 'POST') {
-        
-        //storing in a database: Mongodb
-        const client = await MongoClient.connect(
-            'mongodb+srv://crud:crud123@web.gq8l24z.mongodb.net/?retryWrites=true&w=majority'
-        );
-        const db = client.db('core');//getting access to the database
-        //access to a collection(table) and queries as 'isertOne'
-        await db.collection('tasks').insertOne(newTask);
-        client.close();
-        res.status(201).json({ message: 'succesfully POST request in tasks' });
-    }
+    //storing in a database: Mongodb
+    const db = client.db('core');//getting access to the database
+    //access to a collection(table) and queries as 'isertOne'
+    await db.collection('tasks').insertOne(newTask);
+    client.close();
+    res.status(201).json({ message: 'succesfully POST request in tasks' });
+  }
+
+
+  client.close();
 
 }
 
